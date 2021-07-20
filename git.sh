0295fb4bf31721ced7a0bcfbfea40be3938265da
WarrenHewitt <warrenemb@163.com>
2021-07-20 10:42:06 +0800

    regular update

--- git.sh
+++ git.sh
@@ -5,17 +5,21 @@ set -e
 
 method=${1:-"pull"} # 设置默认参数 注意 $1 的写法，:- 表示为空或未定义  只有 - 没有 : 表示判断没有定义
 
-git add -A
-git commit -m 'regular update'
+{ 
+    git add -A
+    git commit -m 'regular update'
+} || {
+    echo 'catch'
+}
 
 echo "commit 完成"
 
 if [ method = "push" ]; then
     echo "git push"
-    git push
+    # git push
 else
     echo "git pull"
-    git pull
+    # git pull
 fi
 
 echo $method "完成"
